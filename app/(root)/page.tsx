b09eaa6af92bd49b658edc89d86eaee2bd1836b5
shivaywadhawan <shivaywadhawan@gmail.com>
2024-07-02 20:10:34 -0400

    Implemented home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = {
+    firstName: "Shivay",
+    lastName: "Wadhawan",
+    email: "contact@contact.com",
+  };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1350.25}
+          />
+        </header>
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactiions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,51 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2 flex">
+          <Image
+            src={"/icons/logo.svg"}
+            alt={"Vertex logo"}
+            width={34}
+            height={34}
+            className="size-[30px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Vertex</h1>
+        </Link>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith("${item.route}/");
+          return (
+            <Link
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+              href={item.route}
+              key={item.label}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
